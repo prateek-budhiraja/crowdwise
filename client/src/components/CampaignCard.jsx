f1dcd360b4e4df9c9a1874120963c39e1d756b1f
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-05-16 23:36:08 +0530

    added campaign card and render on home

--- client/src/components/CampaignCard.jsx
+++ client/src/components/CampaignCard.jsx
@@ -1,5 +1,25 @@
-function CampaignCard() {
-	return <div>CampaignCard</div>;
+function CampaignCard({ campaign }) {
+	const goalPercentage = (campaign.raised / campaign.goal) * 100;
+
+	return (
+		<div className="m-4 rounded-lg border-2 border-gray-300 max-w-[500px]">
+			<img src={campaign.banner} alt="" />
+			<div className="px-2 my-2">
+				<h4 className="text-gray-600 -mb-1 text-sm">{campaign.location}</h4>
+				<h3 className="text-lg font-medium text-gray-300 mt-2 leading-none">
+					{campaign.name}
+				</h3>
+				<h4 className="text-gray-600 mt-1 text-xs">By {campaign.created_by}</h4>
+				<div className="w-full bg-lightGray rounded-full h-2 mt-2">
+					<div
+						className="bg-accentOrange h-2 rounded-full"
+						style={{ width: `${goalPercentage}%` }}
+					></div>
+				</div>
+				<h4 className="text-gray-300 mt-1.5">₹ {campaign.raised} raised</h4>
+			</div>
+		</div>
+	);
 }
 
 export default CampaignCard;
--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,4 +1,17 @@
+import { categories } from "../utils/categories";
+import CampaignCard from "./CampaignCard";
+import CategoryButton from "./CategoryButton";
 import Nav from "./Nav";
+
+const campaign = {
+	banner:
+		"https://img.freepik.com/free-photo/senior-woman-with-walking-frame-hospital-waiting-room-rehabilitation-treatment_482257-8586.jpg?w=740&t=st=1684258449~exp=1684259049~hmac=25d3d1f7bd41d161f076ed046daef70555bcc5fb7612574ed8317759cf2f206b",
+	name: "Austin’s Life-Saving Heart Transplant Fund",
+	location: "LOS ANGELES, CA",
+	created_by: "Austin",
+	goal: 100000,
+	raised: 80000,
+};
 
 function Home() {
 	return (
@@ -11,6 +24,17 @@ function Home() {
 				<button className="mt-5 lg:mt-10 lg:text-xl py-2 px-4 lg:px-6 rounded-full bg-lightGray font-medium text-accentOrange hover:bg-accentOrange hover:text-lightGray">
 					Create a Campaign 🚀
 				</button>
+			</div>
+			<div className="mt-5 py-2 px-8 flex flex-row lg:justify-center overflow-x-scroll scrollbar-hide gap-2">
+				{Object.keys(categories).map((key) => (
+					<CategoryButton category={categories[key]} key={key} />
+				))}
+			</div>
+			<div>
+				<h1 className="underline text-xl font-semibold text-center px-5 mt-8 text-accentOrange">
+					Active Campaigns
+				</h1>
+				<CampaignCard campaign={campaign} />
 			</div>
 		</>
 	);

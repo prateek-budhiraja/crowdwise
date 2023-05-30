1a69b62ee37791507a1194986ebc3ccdd3ccc433
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-05-30 15:14:14 +0530

    added cors

--- server/app.js
+++ server/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import dbConnect from "./config/dbConnect.js";
 import cookieParser from "cookie-parser";
 import homeRouter from "./routes/home.route.js";
@@ -11,6 +12,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+	cors({
+		origin: process.env.CLIENT_URL || "http://localhost:3000",
+		credentials: true,
+	})
+);
 
 app.use("/api", homeRouter);
 app.use("/api/auth", authRouter);

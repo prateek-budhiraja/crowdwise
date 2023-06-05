fc555d08aba97271d0f20dcc6f144f927ee7e327
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-06-05 18:19:52 +0530

    added toasts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,15 +4,22 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import Home from "./components/Home";
 import CategoryPage from "./components/CategoryPage";
 import CampaignPage from "./components/CampaignPage";
+import StartACampaign from "./components/StartACampaign";
+import axios from "axios";
+import config from "./utils/constants.js";
+
+axios.defaults.baseURL = config.SERVER_URL;
+axios.defaults.withCredentials = true;
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
-				<GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_clientId}>
+				<GoogleOAuthProvider clientId={config.GOOGLE_CLIENT_ID}>
 					<Routes>
 						<Route path="/" element={<Home />} />
 						<Route path="/login" element={<Login />} />
+						<Route path="/start-a-campaign" element={<StartACampaign />} />
 						<Route path="/c/:category" element={<CategoryPage />} />
 						<Route path="/browse/:campaign_slug" element={<CampaignPage />} />
 					</Routes>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Toaster } from "react-hot-toast";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<div>
+		<Toaster
+			position="top-right"
+			reverseOrder={false}
+			toastOptions={{
+				className:
+					"bg-gray-300 text-black p-2 border-1 border-gray-400 rounded-full",
+			}}
+		/>
+		<App />
+	</div>
+);

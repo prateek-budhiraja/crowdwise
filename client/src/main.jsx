a5227a38f74564da7017b0783523cbee27b80c5b
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-06-05 18:28:09 +0530

    refactor code

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,17 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import "./index.css";
 import { Toaster } from "react-hot-toast";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<div>
+	<>
 		<Toaster
 			position="top-right"
-			reverseOrder={false}
 			toastOptions={{
 				className:
 					"bg-gray-300 text-black p-2 border-1 border-gray-400 rounded-full",
 			}}
 		/>
 		<App />
-	</div>
+	</>
 );

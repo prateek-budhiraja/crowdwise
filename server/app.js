a9c3eb7f581391175f43e7f984440417eeae821c
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-07-19 22:51:10 +0530

    added admin frontend url to cors

--- server/app.js
+++ server/app.js
@@ -14,9 +14,19 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+const allowedDomains = config.CLIENT_URL.split(",");
 app.use(
 	cors({
-		origin: config.CLIENT_URL,
+		origin: function (origin, callback) {
+			if (!origin) return callback(null, true);
+
+			if (allowedDomains.indexOf(origin) === -1) {
+				var msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+				return callback(new Error(msg), false);
+			}
+			return callback(null, true);
+		},
 		credentials: true,
 	})
 );

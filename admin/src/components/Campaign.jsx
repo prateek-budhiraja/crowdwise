f74b097a6f28c77560a9e488d67d5a07bb8d291c
Prateek Budhiraja <prateekbudhiraja14@gmail.com>
2023-07-20 11:15:20 +0530

    added features to active inactive, approve reject campaign

--- admin/src/components/Campaign.jsx
+++ admin/src/components/Campaign.jsx
@@ -1,39 +1,80 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { toast } from "react-hot-toast";
-import { Link } from "react-router-dom";
 
 const Campaign = () => {
 	const [campaigns, setCampaigns] = useState([]);
 
+	const fetchCampaigns = async () => {
+		try {
+			const { data } = await axios.get("/api/campaigns");
+			if (data?.success) setCampaigns(data.data);
+			else throw new Error("Error fetching campaigns");
+			console.log(data);
+		} catch (error) {
+			toast.error(error.message);
+		}
+	};
+
 	useEffect(() => {
-		const fetchCampaigns = async () => {
-			try {
-				const { data } = await axios.get("/api/campaigns");
-				if (data?.success) setCampaigns(data.data);
-				else throw new Error("Error fetching campaigns");
-			} catch (error) {
-				toast.error(error.message);
-			}
-		};
 		fetchCampaigns();
 	}, []);
 
-	const approve = () => {
-		console.log("Approve");
+	const approve = async (slug) => {
+		try {
+			const { data } = await axios.put(`/api/admin/campaigns/${slug}/verify`);
+			console.log(data);
+			if (data?.success) toast.success("Campaign approved");
+			else throw new Error("Unable to approve campaign");
+			fetchCampaigns();
+		} catch (error) {
+			console.log(error);
+			toast.error(error.message);
+		}
 	};
 
-	const inactive = () => {
-		console.log("Inactive");
+	const reject = async (slug) => {
+		try {
+			const { data } = await axios.delete(
+				`/api/admin/campaigns/${slug}/reject`
+			);
+			if (data?.success) toast.success(data?.message);
+			else throw new Error(data?.message);
+			fetchCampaigns();
+		} catch (error) {
+			toast.error(error.message);
+		}
+	};
+
+	const inactive = async (slug) => {
+		try {
+			const { data } = await axios.put(`/api/admin/campaigns/${slug}/inactive`);
+			if (data?.success) toast.success(data?.message);
+			else throw new Error(data?.message);
+			fetchCampaigns();
+		} catch (error) {
+			toast.error(error.message);
+		}
+	};
+
+	const active = async (slug) => {
+		try {
+			const { data } = await axios.put(`/api/admin/campaigns/${slug}/active`);
+			if (data?.success) toast.success(data?.message);
+			else throw new Error(data?.message);
+			fetchCampaigns();
+		} catch (error) {
+			toast.error(error.message);
+		}
 	};
 
 	return (
 		<div>
-			<h1>Campaign</h1>
+			<h1 className="my-4 text-3xl font-semibold text-center">Campaign</h1>
 			<div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 ld:gap-8 justify-items-center md:justify-items-stretch px-6 md:px-10 my-8">
 				{campaigns.map((campaign) => (
 					<div key={campaign._id}>
-						<div className="bg-white rounded-lg shadow-lg overflow-hidden h-[500px]">
+						<div className="bg-white rounded-lg shadow-lg overflow-hidden h-[600px] lg:h-[550px]">
 							<img
 								className="w-full h-56 object-cover object-center"
 								src={
@@ -58,18 +99,39 @@ const Campaign = () => {
 									>
 										<button className="bg-gray-600 px-1 rounded">View</button>
 									</a>
-									<button
-										onClick={approve}
-										className="bg-gray-600 px-1 rounded"
-									>
-										Approve
-									</button>
-									<button
-										onClick={inactive}
-										className="bg-gray-600 px-1 rounded"
-									>
-										Inactive
-									</button>
+									{!campaign?.verified ? (
+										<>
+											<button
+												onClick={() => approve(campaign?.slug)}
+												className="bg-gray-600 px-1 rounded"
+											>
+												Approve
+											</button>
+											<button
+												onClick={() => reject(campaign?.slug)}
+												className="bg-gray-600 px-1 rounded"
+											>
+												Reject
+											</button>
+										</>
+									) : (
+										""
+									)}
+									{campaign?.open ? (
+										<button
+											onClick={() => inactive(campaign?.slug)}
+											className="bg-gray-600 px-1 rounded"
+										>
+											Inactive
+										</button>
+									) : (
+										<button
+											onClick={() => active(campaign?.slug)}
+											className="bg-gray-600 px-1 rounded"
+										>
+											Active
+										</button>
+									)}
 								</div>
 							</div>
 						</div>
